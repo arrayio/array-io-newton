7d7dcefca0de3926ed8da2074e09c7dd7e4c151a
Igor Lavrenov <lavrenovio@cyanide.local>
2018-07-15 15:15:50 +0300

    genesis path fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ ldconfig
 mkdir -p /array-io-newton
 cp /dist/* /array-io-newton/
 cd /array-io-newton
-LC_ALL=C witness_node --data-dir=/array-io-newton --api-access=/array-io-newton/api-access.json --log-config=/array-io-newton/log-config.ini
+LC_ALL=C witness_node --data-dir=/array-io-newton --api-access=api-access.json --log-config=log-config.ini
