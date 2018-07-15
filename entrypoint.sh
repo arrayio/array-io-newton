b116e3e9e527ddb56def7206ac2252dcb180c166
Igor Lavrenov <lavrenovio@cyanide.local>
2018-07-15 14:42:18 +0300

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ldconfig
+mkdir -p /array-io-newton
 cp /dist/* /array-io-newton/
 cd /array-io-newton
 LC_ALL=C witness_node --data-dir=/array-io-newton --api-access=/array-io-newton/api-access.json --log-config=/array-io-newton/log-config.ini
